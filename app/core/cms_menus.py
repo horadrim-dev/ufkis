915088e7575e46c5ce7f83f24653c7f4a2549721
horadrim-dev <email.alexss@gmail.com>
2023-02-21 23:15:57 +0700

    add submenus appearance

--- app/core/cms_menus.py
+++ app/core/cms_menus.py
@@ -10,7 +10,8 @@ class IconModifier(Modifier):
     """
     def modify(self, request, nodes, namespace, root_id, post_cut, breadcrumb):
         # only do something when the menu has already been cut
-        if post_cut:
+        if not post_cut: 
+            # assert False, nodes
             # only consider nodes that refer to cms pages
             # and put them in a dict for efficient access
             page_nodes = {n.id: n for n in nodes if n.attr["is_page"]}
@@ -23,7 +24,8 @@ class IconModifier(Modifier):
                 # take the node referring to the page
                 node = page_nodes[page.id]
                 # put the changed_by attribute on the node
-                node.attr["icon"] = page.iconextension.fa_icon
+                if hasattr(page, 'iconextension'):
+                    node.attr["icon"] = page.iconextension.fa_icon
 
         return nodes
 
