e739f9af492bf1a9b0963ac9d359c9eff906932d
horadrim-dev <email.alexss@gmail.com>
2023-03-13 11:40:17 +0700

    fix

--- app/app/settings.py
+++ app/app/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
-print(DEBUG)
+
 ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 INSTALLED_APPS = [
