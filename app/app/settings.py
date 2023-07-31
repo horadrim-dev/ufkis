8cb609a98cd2443946e3d609e450d85350fb0884
horadrim-dev <email.alexss@gmail.com>
2023-07-31 20:16:41 +0700

    debug commit 2

--- app/app/settings.py
+++ app/app/settings.py
@@ -180,7 +180,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 if DB_IS_AVAIL:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql",
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
             "NAME": DB_DATABASE,
             "USER": DB_USERNAME,
             "PASSWORD": DB_PASSWORD,
