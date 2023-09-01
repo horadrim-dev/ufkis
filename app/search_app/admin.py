91b2e67d59a56565aa154571bff56ce797f61855
horadrim-dev <email.alexss@gmail.com>
2023-09-01 11:22:28 +0700

    delete example app

--- app/search_app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from search_app.models import Document
-
-admin.site.register(Document)
--- app/search_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Document(models.Model):
-    name = models.CharField(max_length=40)
-    body = models.TextField()
-
-    def __unicode__(self):
-        return self.name
