9a90a017e7ba905aea6f230e76da00a19848ae90
horadrim-dev <email.alexss@gmail.com>
2023-09-17 13:19:56 +0700

    add activity & departments to search

--- app/structure/search_indexes.py
+++ app/structure/search_indexes.py
@@ -1,13 +1,27 @@
 from haystack import indexes
-from .models import Organization
+from .models import Organization, Activity, Department, Otdel
+
 
 
 class OrganizationIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     name = indexes.CharField(model_attr='name', null=True)
     short_name = indexes.CharField(model_attr='short_name', null=True)
-    # short_description = indexes.CharField(model_attr='short_description', null=True)
-    # description = indexes.CharField(model_attr='description', null=True)
 
     def get_model(self):
         return Organization
+
+
+class ActivityIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    name = indexes.CharField(model_attr='name', null=True)
+
+    def get_model(self):
+        return Activity
+
+class DepartmentIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    name = indexes.CharField(model_attr='name', null=True)
+
+    def get_model(self):
+        return Department
