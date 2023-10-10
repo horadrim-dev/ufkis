93e4352788a4485b81e309b496e60e4d2d672b64
horadrim-dev <email.alexss@gmail.com>
2023-10-10 15:24:43 +0700

    add events to search

---
+++ app/events/search_indexes.py
@@ -0,0 +1,17 @@
+from haystack import indexes
+from .models import DayEvent
+
+
+class DayEventIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    name = indexes.CharField(model_attr='name', null=True)
+    description = indexes.CharField(model_attr='description', null=True)
+    place = indexes.CharField(model_attr='place', null=True)
+    start_at = indexes.DateTimeField(model_attr='start_at', null=True)
+
+    def get_model(self):
+        return DayEvent
+
+    def index_queryset(self, using=None):
+        "Used when the entire index for model is updated."
+        return self.get_model().objects.upcoming()
